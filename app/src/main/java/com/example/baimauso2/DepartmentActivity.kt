057534cab294a9f179076027af5bf657e4dcdb3e
jenniferANT <nghuynhduyenjenf@gmail.com>
2024-03-18 20:15:29 +0700

    UI: add

---
+++ app/src/main/java/com/example/baimauso2/DepartmentActivity.kt
@@ -0,0 +1,11 @@
+package com.example.baimauso2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DepartmentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_department)
+    }
+}
